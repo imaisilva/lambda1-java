0e6de16a1950e5d8aaa6b57f1797af3967eaf5ef
Mayara Silva <alohasp@outlook.com>
2023-11-24 15:25:00 -0300

    Objeto de classe anonima

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -15,8 +16,14 @@ public class Program {
 		list.add(new Product("Notebook", 1200.0));
 		list.add(new Product("Tablet", 400.0));
 
+		Comparator <Product> comp = new Comparator <Product>() {
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		};
 
-		list.sort(new MyComparator());
+		list.sort(comp);
 		for (Product prod : list) {
 			System.out.println(prod);
 		}
