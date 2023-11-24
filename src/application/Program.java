3fced6ec4612fe07fb217d9dae39568ab418a434
Mayara Silva <alohasp@outlook.com>
2023-11-24 15:32:25 -0300

    Expressao lambda direto no argumento

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,9 +16,7 @@ public class Program {
 		list.add(new Product("Notebook", 1200.0));
 		list.add(new Product("Tablet", 400.0));
 
-		Comparator <Product> comp = (p1, p2) ->p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-
-		list.sort(comp);
+		list.sort((p1, p2) ->p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 		for (Product prod : list) {
 			System.out.println(prod);
 		}
